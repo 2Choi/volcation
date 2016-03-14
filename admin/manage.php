0a78e68e23d2a9a073de39c91d644997473d1a2d
DevSusu <os1742@gmail.com>
2016-03-14 10:33:45 +0900

    finished number module integration

--- admin/manage.php
+++ admin/manage.php
@@ -13,45 +13,43 @@ require_once('../config/config.php');
 				<tr>
 					<td>번호</td>
 					<td>아이디</td>
-					<td>마일리지</td>
 					<td>방문수</td>
+					<td>마일리지</td>
 				</tr>
 <?php
-
-	if(isset($_SESSION['admin']) && $_SESSION['admin'] == true){
-		$sql = "SELECT username, user_id, visit,mileage FROM users";
-
-		$result = $conn->query($sql);
-
-		while($row = $result->fetch()) {
-
-			echo "<tr>";
-			echo "<td>".$row["user_id"]."</td>";
-			echo "<td>".$row["username"]."</td>";
-			echo "<td>";
-			echo '<a href="">'.$row["mileage"].'</a>';
-			echo "</td>";
-			echo "<td>".$row["visit"]."</td>";
-			
-
-			echo "<td>";
-			/// 
-			echo '<div class="number-container">';
-    		echo '<input class="mileage" type="number" value="'.$row['mileage'].'"/>';
-			include("number.php");
-			echo "</td>";
-			echo "</tr>";
-		}
-	}
-	else
-	{
-		echo "jashfjah";
+	if(!isset($_SESSION['admin']) || $_SESSION['admin'] == false) {
 		echo "
 			<script>
 			location.replace(\"login.php\");
 			</script>";
+		exit;
 	}
-	
+
+	if(isset($_POST['submit'])){
+
+		$_POST = array_map( 'stripslashes', $_POST );
+			//collect form data
+		extract($_POST);
+
+		$sql = "UPDATE users SET mileage={$mileage} WHERE user_id={$user_id}";
+		$conn->query($sql);
+
+	}
+
+	$sql = "SELECT username, user_id, visit,mileage FROM users";
+	$result = $conn->query($sql);
+
+	while($row = $result->fetch()) {
+		echo "<tr>";
+		echo "<td>".$row["user_id"]."</td>";
+		echo "<td>".$row["username"]."</td>";
+		echo "<td>".$row["visit"]."</td>";
+
+		echo "<td>";
+			include("number.php");
+		echo "</td>";
+		echo "</tr>";
+	}
 
 ?>
 			</table>
--- admin/number.php
+++ admin/number.php
@@ -1,3 +1,9 @@
+<div class="number-container">
+  <form action="manage.php" method="post">
+  <?php
+    echo '<input class="mileage" name="mileage" type="number" value="'.$row['mileage'].'"/>';
+    echo '<input type="hidden" name="user_id" value="'.$row['user_id'].'"/>';
+  ?>
     <div class="number-control" value="1000000">
       <button type="button" class="plus">+</button>
       <p>1000000</p>
@@ -18,4 +24,6 @@
       <p>1000</p>
       <button type="button" class="minus">-</button>
     </div> <!-- 1000 -->
-  </div>
+    <input class="button button-small button-primary" type="submit" name="submit" value="저장"/>
+  </form>
+</div>
